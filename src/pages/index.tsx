d6038391feb6944ed61d03f92c4e9e7ffb69138c
Jose Costa <josecostasantos.jr@gmail.com>
2023-03-21 23:19:55 -0300

    chore: next-shop

---
+++ src/pages/index.tsx
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import 'keen-slider/keen-slider.min.css';
+import { useKeenSlider } from 'keen-slider/react';
+
+
+import { GetStaticProps } from "next";
+import Stripe from "stripe";
+import { stripe } from "../lib/stripe";
+
+interface HomeProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
+
+  const [sliderRef] = useKeenSlider({
+    mode: "free-snap",
+    slides: {
+      perView: 3,
+      // origin: "center",
+      spacing: 48,
+
+    }
+  })
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map(product => (
+        <Product
+          href={`/product/${product.id}`}
+          key={product.id}
+          className="keen-slider__slide"
+        >
+          <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <footer>
+            <strong>{product.name}</strong>
+            <span>{product.price}</span>
+          </footer>
+        </Product>
+      ))}
+
+    </HomeContainer>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await stripe.products.list({
+    expand: ["data.default_price"]
+  });
+
+  const products = response.data.map(product => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: new Intl.NumberFormat('pt-BR', {
+        currency: "BRL",
+        style: "currency"
+      }).format(Number(price.unit_amount) / 100)
+    }
+  })
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60 * 60 * 2 // Acada 2 horas o next vai criar uma nova versao HTML estatica desta paginae armazenar em cache 
+  }
+}
